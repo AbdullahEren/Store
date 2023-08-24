84eba24139277b4925e794bfbae3bab3abb78ada
abdullaheren <a.eyurek@hotmail.com>
2023-08-24 10:05:20 +0300

    Adding FormFile Operations

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -7,6 +7,8 @@ using Services.Contracts;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<RepositoryContext>(options => 
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
@@ -36,6 +38,8 @@ app.UseEndpoints(endpoints =>
         pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}"
     );
     endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+
 });
 
 
